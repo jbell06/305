36257a3696930d63bf09683d96bfdaa4929534d1
jenayabell <bellj0618@students.bowiestate.edu>
2023-11-10 13:39:52 -0500

    Lab 4 files

---
+++ Lab4.py
@@ -0,0 +1,15 @@
+for count in range(11):
+    print(count)
+for count in range(1,11):
+    print(count)
+
+for number in range(1,11,2):
+    print(number)
+import math
+radius=float(input('Enter the radius of the circle'))
+area=math.pi*(radius**2)
+print(area)
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area=(length*width)
+print(area)
