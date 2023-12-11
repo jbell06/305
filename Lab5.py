7dbfc22ed733d0a98f815f5f5ecffe39e4677e1e
jenayabell <bellj0618@students.bowiestate.edu>
2023-12-11 17:58:01 -0500

    Lab 4 files

---
+++ Lab5.py
@@ -0,0 +1,28 @@
+numbers = int (input("Enter numbers:"))
+for numbers in range(1, numbers):
+    if numbers % 5 == 0 and numbers % 3 == 0
+        print (numbers,"Tic Tac")
+    if numbers % 3 == 0?
+        print (numbers, "Tic")
+    if numbers % 5 == 0:
+        print (numbers, "Tac")
+
+***('In applying the while loop, we count from 1 and 15.'
+    'lower boundary is set at 1 and upper boundary at 16')***
+i= 1
+while i < 16:
+    print (i)
+    i= i + 1
+else:
+    print("Finish")
+
+x = int(input("Enter number:"))
+x = 1
+while x < 16:
+    x = x + 1
+    if x % 5== 0 and x % 3 == 0:
+        print(x, "Tic Tac")
+    elif(x % 3 == 0):
+        print( x, "Tic")
+    elif x % 5 == 0:
+        print(x, "Tac")
